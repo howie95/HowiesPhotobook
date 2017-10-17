f293a4e01e7b14814819f28b63c7d36a76c32197
howie95 <1096046927@qq.com>
2017-10-17 11:16:38 +0800

    add error tips

--- server/database.js
+++ server/database.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise 
-mongoose.connect('mongodb://localhost:27017/photobook')
+mongoose.connect('mongodb://localhost:27017/photobook',{useMongoClient: true},function(err){
+    if(err){console.log("数据库连接失败！")}
+    else{console.log("数据库连接成功！")}
+})
 
 const photoSchem = new mongoose.Schema({
     title: String,
